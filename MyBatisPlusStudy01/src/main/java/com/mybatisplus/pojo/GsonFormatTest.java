7199aebd1cd54715f877da7e5d7281d2953a043f
jpzhou <277124487@qq.com>
2022-08-26 17:03:21 +0800

    分支

--- MyBatisPlusStudy01/src/main/java/com/mybatisplus/pojo/GsonFormatTest.java
+++ MyBatisPlusStudy01/src/main/java/com/mybatisplus/pojo/GsonFormatTest.java
@@ -3,6 +3,7 @@ package com.mybatisplus.pojo;
 public class GsonFormatTest {
     private void print() {
         System.out.println("This is a master!");
+        System.out.println("This is a dev!");
     }
 
 }
